cfdb8d0a5619e1fce191c7438645023f2d53e253
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-04 01:15:23 +0530

    fix bugs and typos

--- Guess_the_number_game_by_computer.py
+++ Guess_the_number_game_by_computer.py
@@ -1,20 +1,26 @@
 import random
+import time
+
 def computer_guess(x,y):
     low=lr
     high=ur
     feedback=''
     while feedback !='c':
-        guess=random.randint(low, high)
+        if low!=high:
+            guess=random.randint(low, high)
+        else:
+            guess=low
         feedback=input(f"Is {guess} too high(h), too low(l) or correct(c)?")
-        if feedback == 'h':
-            high = guess - 1
-        elif feedback == 'l':
-            low = guess + 1
-    print(f"Yay! The computer guessed your number {guess} correctly!!!")
+        if feedback=='h':
+            high=guess-1
+        elif feedback=='l':
+            low=guess+1
+    print(f"Yay! The computer guessed the number {guess} correctly !!!")
 
-lr=int(input("Enter a range between which you what to guess a random number: lower limit:"))
-ur=int(input("Enter a range between which you what to guess a random number: upper limit:"))
+print("Please think of a number in your mind ( Don't mention it! )")
+time.sleep(4)
+
+lr=int(input("Enter lower limit of a range between which the computer has to guess your number:"))
+ur=int(input("Enter upper limit of a range between which the computer has to guess your number:"))
 
 computer_guess(lr,ur)
-
-
